abea3c71727d23bfe5001ba33f14801d0e95d9ec
Thomas John Helin <thomashelin@yahoo.com>
2020-03-30 17:36:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,4 @@
 require "pry"
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -13,6 +9,7 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
 def won?(board)
     WIN_COMBINATIONS.detect do |index|
       board[index[0]] == board[index[1]] &&
@@ -39,4 +36,68 @@ def winner(board)
  winning_combo = won?(board)
  return board[winning_combo[0]]
 end
+end
+
+def turn_count(board)
+counter = 0
+board.each do |position|
+  if position == ("X") || position == ("O")
+counter += 1
+ end
+end
+counter
+end #returns the number of turns that have been played
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+  return "X"
+else
+  return "O"
+end
+end
+
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+ counter = 0
+while counter < 9
+ turn(board)
+ counter += 1
+ display_board(board)
+ end
 end
